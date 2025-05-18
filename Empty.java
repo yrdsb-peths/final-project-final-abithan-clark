3713e69a1d104cec07083ef1555be0bec417ba0d
AbithanP <abithan.mayo@gmail.com>
2025-05-18 19:27:12 -0400

    add Background and squares

---
+++ Empty.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Empty here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Empty extends Actor
+{
+    public Empty()
+    {
+        setImage("images/empty.png");
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+        
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,24 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super(500, 500, 1);
+        
+        //setting the first square y position
+        int posY = 70;
+        
+        for (int x = 0; x < 4; x++)
+        {
+            //resetting the square y position
+            int posX = 70;
+            for (int i = 0; i < 4; i++)
+            {
+                //create 4 new empty objects per row
+                Empty empty = new Empty();
+                addObject(empty, posX, posY);
+                posX += 120;
+            }
+            //lowers the next row
+            posY += 120;
+        }
     }
 }
