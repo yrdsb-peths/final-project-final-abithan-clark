9f7d3e25a6ca234f40749c073be031d409f88a53
ClarkkCC <Clark113008@gmail.com>
2025-05-20 14:08:34 -0400

    added 8 & 16

---
+++ Eight.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Eight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Eight extends Actor
+{
+    public Eight()
+    {
+        setImage("images/eight.png");
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
