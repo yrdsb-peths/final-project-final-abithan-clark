37e0e7897a2f6d67db45fff0e4fe13a188fef90b
AbithanP <abithan.mayo@gmail.com>
2025-06-10 22:39:05 -0400

    Shorten instructions and code

--- TitleScreen.java
+++ TitleScreen.java
@@ -32,28 +32,11 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld); 
         }
+        
         if(Greenfoot.mouseClicked(infoLabel))
         {
             InstructionScreen infoScreen = new InstructionScreen();
             Greenfoot.setWorld(infoScreen);
-        }
-        
-        if(Greenfoot.mouseClicked(infoLabel))
-        {
-            TextBg infoBg = new TextBg(480, 250);
-            
-            /*
-             * If a block value of 256 is created, hard mode will start, and now 2 blocks will be
-             * created when the arrow keys are pressed. If a block value of 512 is created, 3 new
-             * blocks will now spawn with each movement. 
-            */
-           
-            Label info = new Label("the the the the \n the the the thet ehth ", 50);
-            X infoX = new X();
-            
-            addObject(infoBg, 268, 160);
-            addObject(info, 268, 250);
-            addObject(infoX, 70, 245);
         }
     }
 }
